079049821bf8602cb2e6dfd7ebe26615b60c4365
MatLaser2 <matlaser.8128@gmail.com>
2021-09-08 08:20:25 +0900

    change encode from utf-8 to shift-jis

--- makeDir.bat
+++ makeDir.bat
@@ -1,13 +1,14 @@
 @echo off
   
-set /p foldername="フォルダ名を入力："
-set /p number="フォルダ数を入力："
+set /p foldername="input directory name : "
+set /p start_number="input start number : "
+set /p end_number="input end number : "
   
 setlocal enabledelayedexpansion
   
-for /l %%i in (1,1,%number%) do (
-  set num=0%%i
-  set num=!num:~-2,2!
+for /l %%i in (%start_number%,1,%end_number%) do (
+  set num=00%%i
+  set num=!num:~-3,3!
   mkdir %foldername%!num!
 )
   
--- makeFile.bat
+++ makeFile.bat
@@ -1,14 +1,15 @@
 @echo off
   
-set /p filename="ファイル名を入力："
-set /p extension="拡張子を入力："
-set /p number="ファイル数を入力："
+set /p filename="input file name : "
+set /p extension="input extension : "
+set /p start_number="input start number : "
+set /p end_number="input end number : "
   
 setlocal enabledelayedexpansion
   
-for /l %%i in (1,1,%number%) do (
-  set num=0%%i
-  set num=!num:~-2,2!
+for /l %%i in (%start_number%,1,%end_number%) do (
+  set num=00%%i
+  set num=!num:~-3,3!
   type nul > %filename%!num!.!extension!
 )
   
